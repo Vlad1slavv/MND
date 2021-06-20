7f54ccc1e21d6d8ec608f980d6d644dafe6b0aa1
Vlad1slavv <66327959+Vlad1slavv@users.noreply.github.com>
2021-06-20 19:24:17 +0300

    Update main.py

--- mnd5/main.py
+++ mnd5/main.py
@@ -1,14 +1,11 @@
 import random
-from sklearn.linear_model import LinearRegression
+import sklearn.linear_model as lm
+import numpy as np
+import time
 from scipy.stats import f, t
 from functools import partial
 from pyDOE2 import *
-from prettytable import PrettyTable
-from time import time
-
-x_range = ((-1, 4), (-3, 6), (-1, 9))
-y_min = 200 + int(sum([x[0] for x in x_range]) / 3)
-y_max = 200 + int(sum([x[1] for x in x_range]) / 3)
+from beautifultable import BeautifulTable
 
 
 def plan_matrix(n, m):
@@ -17,42 +14,38 @@ def plan_matrix(n, m):
         for j in range(m):
             y[i][j] = random.randint(y_min, y_max)
 
-    x_norm = ccdesign(3, center=(0, 1))
-    permutation = [2, 1, 0]
-    idx = np.empty_like(permutation)
-    idx[permutation] = np.arange(len(permutation))
-    x_norm = x_norm[:, idx]
-
+    if n > 14:
+        no = n - 14
+    else:
+        no = 1
+    x_norm = ccdesign(3, center=(0, no))
     x_norm = np.insert(x_norm, 0, 1, axis=1)
 
     for i in range(4, 11):
         x_norm = np.insert(x_norm, i, 0, axis=1)
 
     l = 1.215
-    x_norm[8][1] = -l
-    x_norm[9][1] = l
-    x_norm[8][3] = 0
-    x_norm[9][3] = 0
-    x_norm[10][2] = -l
-    x_norm[11][2] = l
-    x_norm[12][3] = -l
-    x_norm[13][3] = l
-    x_norm[12][1] = 0
-    x_norm[13][1] = 0
+    for i in range(len(x_norm)):
+        for j in range(len(x_norm[i])):
+            if x_norm[i][j] < -1 or x_norm[i][j] > 1:
+                if x_norm[i][j] < 0:
+                    x_norm[i][j] = -l
+                else:
+                    x_norm[i][j] = l
 
-    def fill_tabs(x):
+    def add_sq_nums(x):
         for i in range(len(x)):
             x[i][4] = x[i][1] * x[i][2]
             x[i][5] = x[i][1] * x[i][3]
             x[i][6] = x[i][2] * x[i][3]
             x[i][7] = x[i][1] * x[i][3] * x[i][2]
-            x[i][8] = round(x[i][1] ** 2, 3)
-            x[i][9] = round(x[i][2] ** 2, 3)
-            x[i][10] = round(x[i][3] ** 2, 3)
+            x[i][8] = x[i][1] ** 2
+            x[i][9] = x[i][2] ** 2
+            x[i][10] = x[i][3] ** 2
         return x
 
-    x_norm = fill_tabs(x_norm)
-    x = np.ones(shape=(len(x_norm), len(x_norm[0])), dtype=np.float64)
+    x_norm = add_sq_nums(x_norm)
+    x = np.ones(shape=(len(x_norm), len(x_norm[0])), dtype=np.int64)
     for i in range(8):
         for j in range(1, 4):
             if x_norm[i][j] == -1:
@@ -61,45 +54,27 @@ def plan_matrix(n, m):
                 x[i][j] = x_range[j - 1][1]
 
     for i in range(8, len(x)):
-        for j in range(1, 4):
+        for j in range(1, 3):
             x[i][j] = (x_range[j - 1][0] + x_range[j - 1][1]) / 2
-    dx = [(x_range[i][1] - x_range[i][0]) / 2 for i in range(3)]
-    x[8][1] = -l * dx[0] + x[9][1]
-    x[9][1] = l * dx[0] + x[9][1]
-    x[10][2] = -l * dx[1] + x[9][2]
-    x[11][2] = l * dx[1] + x[9][2]
-    x[12][3] = -l * dx[2] + x[9][3]
-    x[13][3] = l * dx[2] + x[9][3]
-    x = np.around(fill_tabs(x), 3)
-
-    x_table = PrettyTable()
+    dx = [x_range[i][1] - (x_range[i][0] + x_range[i][1]) / 2 for i in range(3)]
+    x[8][1] = l * dx[0] + x[9][1]
+    x[9][1] = -l * dx[0] + x[9][1]
+    x[10][2] = l * dx[1] + x[9][2]
+    x[11][2] = -l * dx[1] + x[9][2]
+    x[12][3] = l * dx[2] + x[9][3]
+    x[13][3] = -l * dx[2] + x[9][3]
+    x = add_sq_nums(x)
+    x_table = BeautifulTable()
     for i in range(n):
-        x_table.add_row([*x[i]])
-    print('Матриця планування:')
+        x_table.rows.append([*x[i]])
+    print('Х matrix:')
     print(x_table)
-
-    x_norm_table = PrettyTable()
+    x_norm_table = BeautifulTable()
     for i in range(n):
-        x_norm_table.add_row([*x_norm[i]])
-    print('Нормована матриця планування:')
+        x_norm_table.rows.append([*x_norm[i]])
+    print('Normalized x matrix:')
     print(x_norm_table)
-
     return x, y, x_norm
-
-
-def coef_finding(x, y, norm=False):
-    skm = LinearRegression(fit_intercept=False)
-    skm.fit(x, y)
-    b = skm.coef_
-    if norm == 1:
-        print('\nКоефіцієнти з нормаваними значеннями:')
-    else:
-        print('\nКоефіцієнти:')
-    b = [round(i, 3) for i in b]
-    print(b)
-    print(y)
-    print('\nЗначення рівняння зі знайденими коефіцієнтами:\n{}'.format(np.dot(x, b)))
-    return b
 
 
 def regression(x, b):
@@ -115,95 +90,129 @@ def s_kv(y, y_aver, n, m):
     return res
 
 
-def checkFull(x, y, b, n, m):
-    print('\nСтатистичні перевірки:')
-    f1 = m - 1
-    f2 = n
-    f3 = f1 * f2
-    q = 0.05
+def coef_finding(x, y, norm=False):
+    skm = lm.LinearRegression(fit_intercept=False)
+    skm.fit(x, y)
+    b = skm.coef_
+    if norm == 1:
+        print('\nКоефіцієнти рівняння регресії з нормованим x:')
+    else:
+        print('\nКоефіцієнти рівняння регресії:')
+    b = [round(i, 3) for i in b]
+    print(b)
+    print('\nРезультат рівняння з знайденими коефіцієнтами:\n{}'.format(np.dot(x, b)))
+    return b
 
-    kohren = {2: 3346, 3: 2758, 4: 2419, 5: 2159, 6: 2034, 7: 1911, 8: 1815, 9: 1736, 10: 1671}
-    g_kr = kohren.get(f1) / 10000
 
-    student = partial(t.ppf, q=1 - q)
-    t_student = student(df=f3)
-
-    y_aver = [round(sum(i) / len(i), 3) for i in y]
-    print('\nСередні значення Y:', y_aver)
-    disp = s_kv(y, y_aver, n, m)
-    print('Дисперсія Y:', disp)
-    ck = time()
-
+def kohren_kr(y, y_aver, n, m):
     f1 = m - 1
     f2 = n
     q = 0.05
     skv = s_kv(y, y_aver, n, m)
     gp = max(skv) / sum(skv)
+    print('\nПеревірка  за Кохреном')
+    return gp
 
-    ck = time() - ck
-    print(f'\nКритерій Кохрена:\ngp = {gp}')
-    if gp < g_kr:
-        print('Дисперсія однорідна')
-    else:
-        print("Дисперсія неонорідна")
-        m += 1
-        start(n, m)
-    cs = time()
+
+def kohren(f1, f2, q=0.05):
+    q1 = q / f1
+    fisher_value = f.ppf(q=1 - q1, dfn=f2, dfd=(f1 - 1) * f2)
+    return fisher_value / (fisher_value + f1 - 1)
+
+
+def bs(x, y_aver, n):
+    res = [sum(1 * y for y in y_aver) / n]
+    for i in range(len(x[0])):
+        b = sum(j[0] * j[1] for j in zip(x[:, i], y_aver)) / n
+        res.append(b)
+    return res
+
+
+def student_kr(x, y, y_aver, n, m):
     skv = s_kv(y, y_aver, n, m)
     skv_aver = sum(skv) / n
     sbs_tmp = (skv_aver / n / m) ** 0.5
-
-    def bs(x, y_aver, n):
-        res = [sum(1 * y for y in y_aver) / n]
-        for i in range(len(x[0])):
-            b = sum(j[0] * j[1] for j in zip(x[:, i], y_aver)) / n
-            res.append(b)
-        return res
-
-    bs_tmp = bs(x[:, 1:], y_aver, n)
+    bs_tmp = bs(x, y_aver, n)
     ts = [round(abs(b) / sbs_tmp, 3) for b in bs_tmp]
+    return ts
 
-    cs = time() - cs
-    print('\nКритерій Стьюдента:\n{}:'.format(ts))
-    res = [t for t in ts if t > t_student]
-    final_k = [b[i] for i in range(len(ts)) if ts[i] in res]
-    print('\nКоефіцієнти {} незначимі'.format([round(i, 3) for i in b if i not in final_k]))
-    y_new = []
-    for j in range(n):
-        y_new.append(round(regression([x[j][i] for i in range(len(ts)) if ts[i] in res], final_k), 3))
-    print('Значення функції відгуку зі значимими коефіцієнтами {}: '.format(final_k))
-    print(y_new)
-    d = len(res)
-    if d >= n:
-        print('\nF4 <= 0')
-        return
-    f4 = n - d
-    cr = time()
+
+def fisher_kr(y, y_aver, y_new, n, m, d):
     S_ad = m / (n - d) * sum([(y_new[i] - y_aver[i]) ** 2 for i in range(len(y))])
     skv = s_kv(y, y_aver, n, m)
     skv_aver = sum(skv) / n
-    f_p = S_ad / skv_aver
+    return S_ad / skv_aver
 
-    fisher = partial(f.ppf, q=1 - q)
+
+def check(x, y, b, n, m):
+    print('\nПеревірка рівння:')
+    f1 = m - 1
+    f2 = n
+    f3 = f1 * f2
+    q = 0.05
+
+    start_time_kohren = time.perf_counter()
+    g_kr = kohren(f1, f2)
+    y_aver = [round(sum(i) / len(i), 3) for i in y]
+    print('\nСереднє значення у:', y_aver)
+    disp = s_kv(y, y_aver, n, m)
+    print('Дисперсія y:', disp)
+    gp = kohren_kr(y, y_aver, n, m)
+    print(f'gp = {gp}')
+    if gp < g_kr:
+        print('З ймовірністю {} дисперсії є однорідними'.format(1 - q))
+    else:
+        print("Необхідно збільшити кількість експериментів")
+        m += 1
+        main(n, m)
+    print(f"Час перевірки однорідності дисперсії за Кохреном: {time.perf_counter()-start_time_kohren}")
+
+    start_time_student = time.perf_counter()
+    student = partial(t.ppf, q=1 - q)
+    t_student = student(df=f3)
+    ts = student_kr(x[:, 1:], y, y_aver, n, m)
+    print('\nКритерій Стюдента:\n{}:'.format(ts))
+    res = [t for t in ts if t > t_student]
+    final_k = [b[i] for i in range(len(ts)) if ts[i] in res]
+    print('\nКоефієнти {} є статистично незначущими, тому ми виключаємо їх із рівняння'.format([round(i, 3) for i in b if i not in final_k]))
+    y_new = []
+    for j in range(n):
+        y_new.append(round(regression([x[j][i] for i in range(len(ts)) if ts[i] in res], final_k), 3))
+    print('Значення y з коефіцієнтами {}: '.format(final_k))
+    print(y_new)
+    d = len(res)
+    print(f"Час перевірки значимості коефіцієнтів регресії за Стьюдентом: {time.perf_counter() - start_time_student}")
+    if d >= n:
+        print('\nF4 <= 0')
+        print('')
+        return
+
+    start_time_fisher = time.perf_counter()
+    f4 = n - d
+    f_p = fisher_kr(y, y_aver, y_new, n, m, d)
+    fisher = partial(f.ppf, q=0.95)
     f_t = fisher(dfn=f4, dfd=f3)
-    cr = time() - cr
-    print('\nКритерій Фішера:')
+    print('\nПеревірка адекватності за Фішером')
     print('fp =', f_p)
     print('ft =', f_t)
     if f_p < f_t:
-        print('Математична модель адекватна')
+        print('Математична модель адекватна експериментальним даним')
     else:
-        print('Математична модель неадекватна')
-    print("час проведення перевірки за критерієм Кохрена ", ck * 1000, " мс")
-    print("час проведення перевірки за критерієм Стьюдента ", cs * 1000, " мс")
-    print("час проведення перевірки за критерієм Фішера ", cr * 1000, " мс")
+        print('Математична модель неадекватна експериментальним даним')
+    print(f"Час перевірки адекватності моделі оригіналу за допомогою критерію Фішера: {time.perf_counter() - start_time_fisher}")
 
 
-def start(n, m):
+def main(n, m):
     x, y, x_norm = plan_matrix(n, m)
-    y_aver = [round(sum(i) / len(i), 3) for i in y]
-    b = coef_finding(x, y_aver)
-    checkFull(x_norm, y, b, n, m)
+    y5_aver = [round(sum(i) / len(i), 3) for i in y]
+    b = coef_finding(x, y5_aver)
+    check(x_norm, y, b, n, m)
 
 
-start(15, 3)
+x_range = ((-2, 5), (-6, 4), (-9, 8))
+x_aver_max = sum([x[1] for x in x_range]) / 3
+x_aver_min = sum([x[0] for x in x_range]) / 3
+y_min = 200 + int(x_aver_min)
+y_max = 200 + int(x_aver_max)
+
+main(15, 3)
